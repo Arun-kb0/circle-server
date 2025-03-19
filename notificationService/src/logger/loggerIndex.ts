c9e4097f8f3f58c345a3365fa141c21a7b8dfd28
Arun <arun11.kb@gmail.com>
2025-03-20 00:14:40 +0530

    post, payment services dockerized & cron job for clearing logs added

--- notificationService/src/logger/loggerIndex.ts
+++ notificationService/src/logger/loggerIndex.ts
@@ -3,38 +3,31 @@ import fs, { promises as fsPromises } from 'fs'
 import { prodLogger } from './prodLogger'
 import { devLogger } from './devLogger'
 import { join } from 'path'
+import corn from 'node-cron'
 
-const DIR_SIZE_LIMIT = 1024 * 1024
-
-const getFolderSize = async (folderPath: string) => {
-  const files = await fsPromises.readdir(folderPath, { withFileTypes: true })
-  let totalSize = 0
-
-  for (const file of files) {
-    const filePath = join(folderPath, file.name)
-    const stat = await fsPromises.stat(filePath)
-    if (stat.isFile()) {
-      totalSize += stat.size
-    }
-  }
-  return totalSize
-}
+const CORN_EXPRESSION = process.env.CORN_EXPRESSION || '0 0 * * *'
 
 const createDir = async () => {
   const curDir = process.cwd()
-  const logDir = join(curDir, '..', '..', 'logs')
-  if (fs.existsSync(logDir)) {
-    const size = await getFolderSize(logDir)
-    if (size > DIR_SIZE_LIMIT) {
-      console.warn('Logs folder exceeds 1 MB. Deleting...');
-      await fsPromises.rm(logDir, { recursive: true, force: true })
-    }
-  }
+  const logDir = join(curDir, 'logs')
   if (!fs.existsSync(logDir)) {
     await fsPromises.mkdir(logDir)
   }
 }
 
+const clearLogDir = async () => {
+  const curDir = process.cwd()
+  const logDir = join(curDir, 'logs')
+  if (fs.existsSync(logDir)) {
+    await fsPromises.rm(logDir, { recursive: true, force: true })
+  }
+}
+
+corn.schedule(CORN_EXPRESSION, async () => {
+  console.log('Running corn job started')
+  await clearLogDir()
+  console.log('Running corn job finished')
+}, { timezone: 'Asia/Kolkata' })
 
 createDir()
 let logger: Logger
