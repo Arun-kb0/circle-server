885550dd7eef0f7f43b08b7f386048998b1ab645
Arun <arun11.kb@gmail.com>
2025-03-27 22:52:00 +0530

    added chat proto types to fix case sensitivity issue on build

---
+++ chatService/src/proto/chat/DeleteRoomMessagesResponse.ts
@@ -0,0 +1,10 @@
+// Original file: src/proto/chat.proto
+
+
+export interface DeleteRoomMessagesResponse {
+  'isDeleted'?: (boolean);
+}
+
+export interface DeleteRoomMessagesResponse__Output {
+  'isDeleted'?: (boolean);
+}
