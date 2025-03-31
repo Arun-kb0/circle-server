5329af22cc89af05433d78152f999148bd5d371c
Arun <arun11.kb@gmail.com>
2025-04-01 03:03:04 +0530

    allowedOrigins new orign added -api,notification

--- apiGateway/src/config/allowOrigins.ts
+++ apiGateway/src/config/allowOrigins.ts
@@ -1,8 +1,9 @@
 export const allowOrigins: string[] = [
   "http://localhost:5173",
+  'http://localhost:4173/',
   'https://api-preprod.phonepe.com',
   'https://mercury-uat.phonepe.com',
   'https://mercury-t2.phonepe.com',
   'https://merchant-simulator.phonepe.com',
-  'https://circle-client-sigma.vercel.app'
+  'https://circle-client-sigma.vercel.app',
 ]
